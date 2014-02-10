15e63620e064a9a3ec6b66355a637528083f5b94
Chenliang Xu <luckyrand@gmail.com>
2014-02-10 14:56:54 -0600

    add deploy-service.sh for downloading and running necessary scripts
    Download Makefile and deploy.sh from github, if they don't exist in the
    work directory. Then pass all the arguments to deploy.sh

---
+++ deploy-service.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## Download Makefile and deploy.sh from github, if they don't exist in
+## the work directory. Then pass all the arguments to deploy.sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Print commands and their arguments as they are executed
+set -x
+
+if [ -e "Makefile" ] ; then
+    echo "Use included Makefile"
+else
+    echo "Download the default Makefile from github"
+    wget https://github.com/luckyrandom/r-deploy-git/raw/master/Makefile.default -O Makefile
+fi
+
+
+if [ -e "deploy.sh" ] ; then
+    echo "Use included Makefile"
+else
+    echo "Download the default Makefile from github"
+    wget https://github.com/luckyrandom/r-deploy-git/raw/master/deploy.sh -O deploy.sh
+fi
+
+chmod u+x deploy.sh && \\
+./deploy.sh "$@"
